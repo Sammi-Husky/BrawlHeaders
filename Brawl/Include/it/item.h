0cf132ff1e4ec0077c90c88b0196f6b2a9f4f85b
Kapedani <kapedanipm@gmail.com>
2022-10-03 20:19:35 -0400

    Made class for BaseItem

---
+++ Brawl/Include/it/item.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class BaseItem {
+    char _spacer[15712];
+
+    public:
+        int getParam(unsigned int param);
+        float getParam1(unsigned int param);
+        float getParamFloat(unsigned int param);
+
+
+};
