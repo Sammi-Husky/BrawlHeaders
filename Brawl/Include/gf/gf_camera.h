826fa954f73f9e34c2b65cc1b0380c913d6c9e7f
Kapedani <kapedanipm@uwaterloo.ca>
2022-10-24 20:02:51 -0400

    Small gfCamera refactor

--- Brawl/Include/gf/gf_camera.h
+++ Brawl/Include/gf/gf_camera.h
@@ -1,17 +1,26 @@
 #pragma once
 
+struct OrthographicParameters {
+    float top;
+    float bottom;
+    float left;
+    float right;
+};
+
+enum CameraProjection {
+    Camera_Projection_Orthogonal = 0x0,
+    Camera_Projection_Perspective = 0x1,
+};
+
 class gfCamera {
 public:
     char _spacer[220];
     float near;
     float far;
     char _spacer2[4];
-    float top;
-    float bottom;
-    float left;
-    float right;
+    OrthographicParameters ortho;
     char _spacer3[8];
-    int cameraPerspective;
+    CameraProjection projection;
     char _spacer4[48];
 
 };
