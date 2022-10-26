bb2d8e42a9ec8d778f50266835c3ddccc17aa654
Sammi Husky <sammi-husky@live.com>
2022-10-26 15:40:47 -0700

    Fix compilation -- broke due to symbols changed by typedefs changing

--- rvl/Include/types.h
+++ rvl/Include/types.h
@@ -1,8 +1,5 @@
-typedef unsigned long long u64;
-typedef signed long long s64;
-
-typedef unsigned long u32;
-typedef signed long s32;
+typedef unsigned int u32;
+typedef signed int s32;
 
 typedef unsigned short u16;
 typedef signed short s16;
