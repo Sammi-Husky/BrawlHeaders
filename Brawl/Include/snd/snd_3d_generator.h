b237e4f238ebe405deaa73628591c0a34d32af0f
Kapedani <kapedanipm@uwaterloo.ca>
2022-10-28 02:08:11 -0400

    Some snd3dGenerator functions

--- Brawl/Include/snd/snd_3d_generator.h
+++ Brawl/Include/snd/snd_3d_generator.h
@@ -1,6 +1,13 @@
 #pragma once
 
+#include <snd/snd_id.h>
+
 class snd3DGenerator {
     void* sound3DActor;
-    virtual ~snd3DGenerator();
+
+    public:
+        snd3DGenerator();
+        virtual ~snd3DGenerator();
+
+        void playSE(SndID sndID, int unk, int unk2, int unk4);
 };
