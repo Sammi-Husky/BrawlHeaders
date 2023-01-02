5d41e21fb3775837ad5d67b1fb819a81829b3373
Kapedani <kapedanipm@uwaterloo.ca>
2023-01-02 04:41:28 -0500

    gmSetRule updates

--- Brawl/Include/gm/gm_set_rule.h
+++ Brawl/Include/gm/gm_set_rule.h
@@ -2,18 +2,31 @@
 
 #include <StaticAssert.h>
 #include <types.h>
+#include <gm/gm_result_info.h>
 
 struct gmSetRule {
-    char _0[0x18];       // 0x0
-    u8 m_spMeleeOption1; // 0x18
-    u8 m_spMeleeOption2; // 0x19
-    u8 m_spMeleeOption3; // 0x1A
-    u8 m_spMeleeOption4; // 0x1B
-    u8 m_spMeleeOption5; // 0x1C
-    u8 m_spMeleeOption6; // 0x1D
-    u8 m_spMeleeOption7; // 0x1E
-    u8 m_spMeleeOption8; // 0x1F
-    char _1[0x68];       // 0x20
+    char _0[0x2];
+    GameRule gameRule : 8;
+    u8 m_timeMinutes;
+    u8 m_stockCount;
+    u8 m_handicap;
+    u8 m_damageRatio; // Multiplied by 10
+    u8 m_stageChoice;
+    u8 m_stockTimeMinutes;
+    u8 m_isTeamAttack;
+    u8 m_allowPause;
+    u8 m_showScoreDisplay;
+    u8 m_showDamageGauge;
+    char _13[11];
+    u8 m_spMeleeSetting1; // 0x18
+    u8 m_spMeleeSetting2; // 0x19
+    u8 m_spMeleeSetting3; // 0x1A
+    u8 m_spMeleeSetting4; // 0x1B
+    u8 m_spMeleeSetting5; // 0x1C
+    u8 m_spMeleeSetting6; // 0x1D
+    u8 m_spMeleeSetting7; // 0x1E
+    u8 m_spMeleeSetting8; // 0x1F
+    char _32[0x68];       // 0x20
 
     STATIC_CHECK(sizeof(gmSetRule) == 0x88)
 };
