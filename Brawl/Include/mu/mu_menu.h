bb81fd550c9c41f943e2273df8fc27106424e31f
Eon <oglesby.mo@gmail.com>
2023-01-22 19:02:29 +0000

    Tidying up of previous commit

--- Brawl/Include/mu/mu_menu.h
+++ Brawl/Include/mu/mu_menu.h
@@ -11,6 +11,7 @@ public:
     static int exchangeSelchkind2SelCharVoice(int id);
     static int exchangeSelCharVoice2SelCharVoiceLengthE(int id);
     static int exchangeSelCharVoice2SelCharVoiceLengthJ(int id);
+    static int exchangeGmCharacterKind2MuStockchkind(int id);
     static int exchangeGmCharacterKind2Something(int id);
     static void startRumbleController(int controller, int rumbleStrength, int rumbleSetting);
 };
