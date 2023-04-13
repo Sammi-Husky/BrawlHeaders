58f296dd8e05617f153952c7552a07986e6d2f44
Sammi Husky <sammi-husky@live.com>
2023-04-12 20:05:49 -0700

    Add alias for proper static_assert instead of hacky workaround

--- utils/include/StaticAssert.h
+++ utils/include/StaticAssert.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifdef __MWERKS__
+#define static_assert(cond, msg) __static_assert(cond, msg)
+#endif
+
 template <bool>
 struct CompileTimeError;
 template <>
@@ -10,8 +14,14 @@ struct CompileTimeError<true> {
 
 #define UNIQUE_NAME(base) CONCAT(base, __LINE__)
 
-#define STATIC_CHECK(expr) \
-    inline void UNIQUE_NAME(StaticCheck)() { ((void)CompileTimeError<(expr) != 0>()); }
+#define STATIC_CHECK(expr)                       \
+    inline void UNIQUE_NAME(StaticCheck)()       \
+    {                                            \
+        ((void)CompileTimeError<(expr) != 0>()); \
+    }
 
-#define PRINT_SIZEOF(typename) \
-    inline void UNIQUE_NAME(PrintSizeOf)() { char(*__kaboom)[sizeof(typename)] = 1; }
+#define PRINT_SIZEOF(typename)                 \
+    inline void UNIQUE_NAME(PrintSizeOf)()     \
+    {                                          \
+        char(*__kaboom)[sizeof(typename)] = 1; \
+    }
