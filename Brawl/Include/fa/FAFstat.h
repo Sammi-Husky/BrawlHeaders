8f4ddacca033f83658f1a92fe575ff5bb00cfcca
Sammi Husky <sammi-husky@live.com>
2023-05-05 20:03:24 -0700

    Add FAFstat and low-level FAT filesystem funcs

--- Brawl/Include/fa/FAFstat.h
+++ Brawl/Include/fa/FAFstat.h
@@ -1,8 +1,16 @@
 #pragma once
 
-struct FAFStat {
+#include <types.h>
+
+struct pfstat {
     int filesize;
-    int unk[0x14];
+    s16 accessDate;
+    s16 modifiedTime;
+    s16 modifiedDate;
+    s16 createdTime;
+    s16 createdDate;
+    u16 unk;
+    char flags;
 };
 
-extern "C" int FAFstat(const char* filepath, FAFStat* st);
+extern "C" int FAFstat(const char* filepath, pfstat* st);
