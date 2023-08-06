a8ac5d3e9494608deee2adc0b5badf6642ccdd5d
Kapedani <kapedanipm@gmail.com>
2023-08-06 13:36:59 -0400

    Updated soKind enums

--- Brawl/Include/so/so_kind.h
+++ Brawl/Include/so/so_kind.h
@@ -3,10 +3,10 @@
 #include <types.h>
 
 enum soKind {
-    SoKind_Invalid = 0xFFFFFFFF,
-    SoKind_Fighter = 0x0,
-    SoKind_Enemy = 0x1,
-    SoKind_Weapon = 0x2,
-    SoKind_Yakumono = 0x3,
-    SoKind_Item = 0x4
+    StageObject_Invalid = 0xFFFFFFFF,
+    StageObject_Fighter = 0x0,
+    StageObject_Enemy = 0x1,
+    StageObject_Weapon = 0x2,
+    StageObject_Yakumono = 0x3,
+    StageObject_Item = 0x4
 };
