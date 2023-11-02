9220f9ba0f8da19231c5e2319ae00c468dd29bd5
Kapedani <kapedanipm@gmail.com>
2023-11-02 03:42:29 -0400

    min2 and max2

--- Brawl/Include/hk/hk_math.h
+++ Brawl/Include/hk/hk_math.h
@@ -1,11 +1,26 @@
+#pragma once
 
 #include <types.h>
 
 namespace hkMath {
     float fabs(float);
-    float min2f(float, float);
-    float max2f(float, float);
     float sqrt(float);
     float acos(float);
     float asin(float);
+
+    template <class T>
+    T min2(T val1, T val2) {
+        if (val2 > val1) {
+            return val1;
+        }
+        return val2;
+    }
+
+    template <class T>
+    T max2(T val1, T val2) {
+        if (val1 > val2) {
+            return val1;
+        }
+        return val2;
+    }
 }
