66e3e5f55bda202871f492ea6e71d46b517e0d6d
Kapedani <kapedanipm@gmail.com>
2024-03-13 21:21:55 -0400

    Updated MuObject

--- Brawl/Include/mu/mu_object.h
+++ Brawl/Include/mu/mu_object.h
@@ -7,13 +7,14 @@
 class MuObject {
 
 public:
-    char unk[0x10];
+    char unk[0xc];
+    nw4r::g3d::ScnMdl* m_scnMdl;
     nw4r::g3d::ScnMdl* m_sceneModel;
     gfModelAnimation* m_modelAnim;
     char unk2[0x44];
 
-    static MuObject* create(nw4r::g3d::ResFile** output, const char* node, char flag, int otherflag, HeapType type);
-    static MuObject* create(nw4r::g3d::ResFile** output, int node, char flag, int otherflag, HeapType type);
+    static MuObject* create(nw4r::g3d::ResFile** modelSource, const char* node, char flag, nw4r::g3d::ResFile** textureSource, HeapType type);
+    static MuObject* create(nw4r::g3d::ResFile** modelSource, int node, char flag, nw4r::g3d::ResFile** textureSource, HeapType type);
     void changeAnimN(const char* animName);
     void changeNodeAnimN(const char* animName);
     void changeClrAnimN(const char* animName);
