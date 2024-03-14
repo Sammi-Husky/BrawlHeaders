9f450175f5403e32062dde84ca540fdd136243d1
Kapedani <kapedanipm@gmail.com>
2024-03-13 21:44:30 -0400

    Updated MuObject

--- Brawl/Include/mu/mu_object.h
+++ Brawl/Include/mu/mu_object.h
@@ -1,20 +1,23 @@
 #pragma once
 #include <gf/gf_model.h>
 #include <memory.h>
+#include <nw4r/g3d/g3d_resmdl.h>
 #include <nw4r/g3d/g3d_resfile.h>
 #include <nw4r/g3d/g3d_scnobj.h>
 
 class MuObject {
 
 public:
-    char unk[0xc];
+    char unk[0x4];
+    nw4r::g3d::ResFile m_resFile;
+    nw4r::g3d::ResMdl m_resMdl;
     nw4r::g3d::ScnMdl* m_scnMdl;
     nw4r::g3d::ScnMdl* m_sceneModel;
     gfModelAnimation* m_modelAnim;
     char unk2[0x44];
 
-    static MuObject* create(nw4r::g3d::ResFile** modelSource, const char* node, char flag, nw4r::g3d::ResFile** textureSource, HeapType type);
-    static MuObject* create(nw4r::g3d::ResFile** modelSource, int node, char flag, nw4r::g3d::ResFile** textureSource, HeapType type);
+    static MuObject* create(nw4r::g3d::ResFile* modelSource, const char* node, char flag, nw4r::g3d::ResFile* textureSource, HeapType type);
+    static MuObject* create(nw4r::g3d::ResFile* modelSource, int node, char flag, nw4r::g3d::ResFile* textureSource, HeapType type);
     void changeAnimN(const char* animName);
     void changeNodeAnimN(const char* animName);
     void changeClrAnimN(const char* animName);
