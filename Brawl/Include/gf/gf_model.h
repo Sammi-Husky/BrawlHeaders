3914ac6a42214e4137ba1eaa84f4e35d9485d689
Kapedani <kapedanipm@gmail.com>
2024-04-09 02:45:43 -0400

    gfModelAnimation functions

--- Brawl/Include/gf/gf_model.h
+++ Brawl/Include/gf/gf_model.h
@@ -8,6 +8,7 @@
 #include <nw4r/g3d/g3d_anmtexsrt.h>
 #include <nw4r/g3d/g3d_anmvis.h>
 #include <nw4r/g3d/g3d_resfile.h>
+#include <nw4r/g3d/g3d_resmdl.h>
 #include <nw4r/g3d/g3d_scnmdl.h>
 #include <types.h>
 
@@ -23,13 +24,35 @@ public:
     nw4r::g3d::AnmObjMatClrRes* m_anmObjMatClrRes;
     nw4r::g3d::AnmObjShpRes* m_anmObjShpRes;
 
+    gfModelAnimation(nw4r::g3d::ResFile* resFile, nw4r::g3d::ResMdl* resMdl, bool doBind, u32 animIndex, HeapType heapType);
+    gfModelAnimation(nw4r::g3d::ResFile* resFile, nw4r::g3d::ResMdl* resMdl, bool doBind, const char* animName, HeapType heapType);
+
     u16 getFrameCount();
     float getFrame();
     void setFrame(float frame);
+    float getUpdateRate();
     void setUpdateRate(float updateRate);
     void setLoop(bool shouldLoop);
+    void setLoopNode(bool shouldLoop);
+    void setLoopVisible(bool shouldLoop);
+    void setLoopTex(bool shouldLoop);
+    void setLoopTexSrt(bool shouldLoop);
+    void setLoopMatCol(bool shouldLoop);
     void unbindNodeAnim(nw4r::g3d::ScnMdl* sceneModel);
+    void unbindVisibleAnim(nw4r::g3d::ScnMdl* sceneModel);
+    void unbindTexAnim(nw4r::g3d::ScnMdl* sceneModel);
+    void unbindTexSrtAnim(nw4r::g3d::ScnMdl* sceneModel);
+    void unbindMatColAnim(nw4r::g3d::ScnMdl* sceneModel);
     void unbindShapeAnim(nw4r::g3d::ScnMdl* sceneModel);
+    void bindNodeAnim(nw4r::g3d::ScnMdl* sceneModel);
+    void bindVisibleAnim(nw4r::g3d::ScnMdl* sceneModel);
+    void bindTexAnim(nw4r::g3d::ScnMdl* sceneModel);
+    void bindTexSrtAnim(nw4r::g3d::ScnMdl* sceneModel);
+    void bindMatColAnim(nw4r::g3d::ScnMdl* sceneModel);
+    void bindShapeAnim(nw4r::g3d::ScnMdl* sceneModel);
+
+    static void unbind(nw4r::g3d::ScnMdl* sceneModel);
+    static void bind(nw4r::g3d::ScnMdl* sceneModel, gfModelAnimation* modelAnim);
 };
 static_assert(sizeof(gfModelAnimation) == 32, "Class is wrong size!");
 
