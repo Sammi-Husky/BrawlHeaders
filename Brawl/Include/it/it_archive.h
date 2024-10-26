14a9bd0f1639d638188d0ef940f343475b745174
Kapedani <kapedanipm@gmail.com>
2024-10-26 03:29:31 -0400

    Made it_create.h and also made itParam in it_archive.h

--- Brawl/Include/it/it_archive.h
+++ Brawl/Include/it/it_archive.h
@@ -1,8 +1,82 @@
 #pragma once
 
-#include <StaticAssert.h>
-#include <it/item.h>
 #include <types.h>
+#include <it/it_create.h>
+
+struct itParam {
+public:
+    enum SizeKind {
+        Size_Small = 0x0,
+        Size_Large = 0x1
+    };
+
+    enum HaveKind {
+        Have_None = 0x0,
+        Have_Get = 0x1,
+        Have_Motion = 0x2,
+        Have_Normal = 0x3,
+        Have_Pickup = 0x4,
+        Have_Grip = 0x5,
+        Have_Plate = 0x6
+    };
+
+    enum ScaleType {
+        Scale_Normal = 0x0,
+        Scale_Fighter = 0x1
+    };
+
+    enum SlopeType {
+        Slope_None = 0x0,
+        Slope_Slant = 0x1,
+        Slope_Slant_Keep_Y_Rot = 0x2,
+        Slope_KeepAngle = 0x3,
+        Slope_Up = 0x4,
+        Slope_Slant_Smooth = 0x5,
+    };
+
+    enum FlashKind {
+        Flash_None = 0x0,
+        Flash_Normal = 0x1,
+    };
+
+    enum CameraKind {
+        Camera_None = 0x0,
+        Camera_Normal = 0x1,
+        Camera_Center = 0x2,
+    };
+
+    enum HitKind {
+        Hit_None = 0x0,
+        Hit_Stop = 0x1,
+        Hit_Fly = 0x2,
+        Hit_Lr = 0x3
+    };
+
+    enum ReflectKind {
+        Reflect_None = 0x0,
+        Reflect_Normal = 0x1
+    };
+
+    enum ShieldKind {
+        Shield_None = 0x0,
+        Shield_Lost = 0x1,
+        Shield_Born = 0x2,
+        Shield_Hop = 0x3
+    };
+
+    enum DeadAreaKind {
+        Dead_Area_None = 0x0,
+        Dead_Area_Remove = 0x1,
+        Dead_Area_Remove_Not_Xlu = 0x2,
+        Dead_Area_All = 0x3
+    };
+
+    enum AttackHitStopKind {
+        Attack_Hit_Stop_None = 0x0,
+        Attack_Hit_Stop_Normal = 0x1,
+        Attack_Hit_Stop_Parent = 0x2
+    };
+};
 
 class itArchive {
 public:
--- Brawl/Include/it/it_value_accesser.h
+++ Brawl/Include/it/it_value_accesser.h
@@ -2,6 +2,7 @@
 
 #include <types.h>
 #include <so/so_value_accesser.h>
+#include <it/it_archive.h>
 
 class itValueAccesser : public soValueAccesser {
 public:
@@ -66,83 +67,11 @@ public:
         Specialized_Param_Int_Reflect_Kind = 0x5b72,
         Specialized_Param_Int_Shield_Kind = 0x5b73,
         Specialized_Param_Int_DeadArea_Kind = 0x5b74,
-        Specialized_Param_Int_Attack_HitStop_Kind = 0x5b75,
+        Specialized_Param_Int_Attack_Hit_Stop_Kind = 0x5b75,
     };
 
     enum ParamIndefinite {
 
-    };
-
-    enum SizeKind {
-        Size_Small = 0x0,
-        Size_Large = 0x1
-    };
-
-    enum HaveKind {
-        Have_None = 0x0,
-        Have_Get = 0x1,
-        Have_Motion = 0x2,
-        Have_Normal = 0x3,
-        Have_Pickup = 0x4,
-        Have_Grip = 0x5,
-        Have_Plate = 0x6
-    };
-
-    enum ScaleType {
-        Scale_Normal = 0x0,
-        Scale_Fighter = 0x1
-    };
-
-    enum SlopeType {
-        Slope_None = 0x0,
-        Slope_Slant = 0x1,
-        Slope_Slant_Keep_Y_Rot = 0x2,
-        Slope_KeepAngle = 0x3,
-        Slope_Up = 0x4,
-        Slope_Slant_Smooth = 0x5,
-    };
-
-    enum FlashKind {
-        Flash_None = 0x0,
-        Flash_Normal = 0x1,
-    };
-
-    enum CameraKind {
-        Camera_None = 0x0,
-        Camera_Normal = 0x1,
-        Camera_Center = 0x2,
-    };
-
-    enum HitKind {
-        Hit_None = 0x0,
-        Hit_Stop = 0x1,
-        Hit_Fly = 0x2,
-        Hit_Lr = 0x3
-    };
-
-    enum ReflectKind {
-        Reflect_None = 0x0,
-        Reflect_Normal = 0x1
-    };
-
-    enum ShieldKind {
-        Shield_None = 0x0,
-        Shield_Lost = 0x1,
-        Shield_Born = 0x2,
-        Shield_Hop = 0x3
-    };
-
-    enum DeadAreaKind {
-        DeadArea_None = 0x0,
-        DeadArea_Remove = 0x1,
-        DeadArea_Remove_Not_Xlu = 0x2,
-        DeadArea_All = 0x3
-    };
-
-    enum AttackHitStopKind {
-        Attack_HitStop_None = 0x0,
-        Attack_HitStop_Normal = 0x1,
-        Attack_HitStop_Parent = 0x2
     };
 
     virtual soParamAccesser* getExtendParamAccesser();
