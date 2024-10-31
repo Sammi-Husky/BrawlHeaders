af05544d4706bc6405dd321a55cfeffa9d38af52
Kapedani <kapedanipm@gmail.com>
2024-10-31 14:13:24 -0400

    Changed names of bits in ipButton

--- Brawl/Include/ip/Input.h
+++ Brawl/Include/ip/Input.h
@@ -9,19 +9,19 @@ union ipButton {
 
     struct {
         unsigned short : 16;
-        bool m_tapJump : 1;
-        bool m_rightTaunt : 1;
-        bool m_leftTaunt : 1;
-        bool m_downTaunt : 1;
-        bool m_sideTaunt : 1;
-        bool m_upTaunt : 1;
+        bool m_flickJump : 1;
+        bool m_appealSR : 1;
+        bool m_appealSL : 1;
+        bool m_appealLw : 1;
+        bool m_appealS : 1;
+        bool m_appealHi : 1;
         bool _6 : 1;
         bool m_cStick : 1;
         bool _8 : 1;
         bool _9 : 1;
         bool _10 : 1;
         bool _11 : 1;
-        bool m_shield : 1;
+        bool m_guard : 1;
         bool m_jump : 1;
         bool m_special : 1;
         bool m_attack : 1;
