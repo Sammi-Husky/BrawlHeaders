a20f9bab810a0c52ef77a12e43ec262a246b2b2f
mparisi20 <mparisi@stevens.edu>
2025-01-05 21:52:31 -0500

    Update soAnimChr

--- Brawl/Include/so/anim/so_anim_chr.h
+++ Brawl/Include/so/anim/so_anim_chr.h
@@ -4,28 +4,31 @@
 #include <nw4r/g3d/g3d_anmchr.h>
 #include <types.h>
 
-struct soAnimChr {
+class soAnimChr {
     nw4r::g3d::AnmObjChrRes* m_anmChrRes;
-    // 0x4
     float m_animFrame;
-    // 0x8
     float m_frame_;
-    // 0xc
     float m_frameSpeedModifier;
-    // 0x10
     float m_rate;
-
     float m_frame;
+    u8 m_loopFlag: 1;
+    u8 m_unkFlag: 1;
 
-    bool m_loopFlag : 1;
-    bool _24_6 : 1;
-    bool _24_5 : 1;
-    bool _24_4 : 1;
-    bool _24_3 : 1;
-    bool _24_2 : 1;
-    bool _24_1 : 1;
-    bool _24_0 : 1;
-
-    char _pad[3];
+    void reinit(bool destroy);
+public:
+    soAnimChr();
+    ~soAnimChr();
+    void reset();
+    void relinquishAnimObj();
+    void updateFrame(float p1);
+    void setFrame(float p1);
+    bool isEnd() const;
+    bool isLooped() const;
+    void setAnimObj(float p1, float p2, nw4r::g3d::AnmObjChrRes *p3, u8 p4, u8 p5);
+    void destroyAnimObj();
+    void disableNode(u32 id);
+    float getEndFrame(nw4r::g3d::AnmObjChrRes* p1) const;
+    void rewind();
+    void turnBack();
 };
-static_assert(sizeof(soAnimChr) == 28, "Class is wrong size!");
+static_assert(sizeof(soAnimChr) == 0x1C, "Class is the wrong size!");
