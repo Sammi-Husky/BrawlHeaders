1173bfa8186386913e024d843f459bf3d44ea3fc
QuickLava <fastmovingvolcanomatter@gmail.com>
2025-04-08 00:33:59 -0700

    Set Up soCollisionShieldModuleImpl
    Also set up some of soCollisionShieldGroup, though most of it is still undocumented.

---
+++ Brawl/Include/so/collision/so_collision_shield_group.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <StaticAssert.h>
+#include <types.h>
+
+class soCollisionShieldGroup {
+public:
+	u8 _00[0x90];
+	float m_posX;
+	u32 _0x94;
+	float m_Lr;
+	float m_hopAngle;
+	float m_attackMul;
+	float m_speedMul;
+	float m_attackLimit;
+	float m_lifeMul;
+	u8 m_isFront;
+	u8 m_isHop;
+	u8 m_isTurn;
+	u8 m_isNoMBall;
+	u8 m_isNoHop;
+};
+static_assert(sizeof(soCollisionShieldGroup) == 0xB8, "Class is wrong size!");
