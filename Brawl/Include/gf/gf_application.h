361beeb32e92d7dc79c13c63d79aa5dab6a9ea68
mparisi20 <mparisi@stevens.edu>
2025-04-15 23:58:03 -0400

    Update gf_application.h to match main.cpp

--- Brawl/Include/gf/gf_application.h
+++ Brawl/Include/gf/gf_application.h
@@ -4,6 +4,12 @@
 #include <gf/gf_callback.h>
 #include <types.h>
 
+struct vcBootParam {
+    u8 unk0[0x54];
+    void init(int argc, const char* argv[]);
+};
+// TODO size assertion
+
 class gfApplication {
 public:
     u8 unk0[0xE4];
@@ -12,8 +18,15 @@ public:
     u16 m_frameRate;
     u8 unkFA[0x1A];
     gfCallBackList m_114;
-    u8 unk118[0x8];
+    u8 unk118[0x4];
+    vcBootParam m_bootParam;
+
+    gfApplication();
+    ~gfApplication();
+    void init();
+    void mainLoop();
+    void exit();
 };
-static_assert(sizeof(gfApplication) == 0x120, "Class is wrong size!");
+static_assert(sizeof(gfApplication) == 0x170, "Class is wrong size!");
 
 extern gfApplication* g_gfApplication;
