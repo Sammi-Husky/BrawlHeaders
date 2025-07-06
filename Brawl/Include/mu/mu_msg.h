9a64074bb2b4e6c5b033b5339c405fb359489a24
Sammi Husky <sammi-husky@live.com>
2025-07-06 02:02:24 -0700

    Update mu_msg and mu_object headers and lst

--- Brawl/Include/mu/mu_msg.h
+++ Brawl/Include/mu/mu_msg.h
@@ -2,6 +2,7 @@
 
 #include <StaticAssert.h>
 #include <ms/ms_message.h>
+#include <mu/mu_object.h>
 
 class MuMsg {
 public:
@@ -12,7 +13,6 @@ public:
     };
 
 private:
-
     class WindowSetting {
         char _0[0x48];
     };
@@ -38,28 +38,38 @@ private:
 public:
     bool allocMsgBuf(u32 msgSizes, u32 numMsgs);
     void setMsgData(void* msgBinData);
-    int getMsgData(u32, void** outUnk);
+    const char* getMsgData(u32 msgIndex, int* msgLength);
     void initWindowSetting(WindowSetting* windowSetting);
     void attachScnMdlSimple(u32 msgIndex, nw4r::g3d::ScnMdlSimple* sceneModel, u32 boneTextIndex, float fontSize);
+    void attachMuObject(u32 lineIndex, MuObject* muObject, const char* format, float f1, float f2, float f3, float f4, float f5);
+    void detachMuObject(MuObject* muObject);
     void setFontWidthModeAuto(u32 msgIndex);
+    void setFontWidthModeProportional(u32 msgIndex);
+    void setFontWidthModeFixed(u32 msgIndex);
     void setAlignMode(u32 msgIndex, AlignMode alignMode);
+    void setVerticalMode(u32 msgIndex, bool isVertical);
     void setFontColor(u32 msgIndex, u8 r, u8 g, u8 b, u8 a);
     void setHSpace(u32 msgIndex, float);
     void setWScale(u32 msgIndex, float);
     void getWScale(u32 msgIndex, float);
     void setHScale(u32 msgIndex, float);
     void getHScale(u32 msgIndex, float);
-    void setDelayPrint(u32, float, float, float, float);
+    void setDelayPrint(u32 msgIndex, float, float, float, float);
     bool isEndDelayPrint(u32 index);
-    void FUN_800b9448(u32 msgIndex, bool);
-    void FUN_800b9460(u32 msgIndex, u8, u8, u8, float);
+    void setEdgeEnable(u32 msgIndex, bool edgeEnable);
+    void setEdge(u32 msgIndex, u8 r, u8 g, u8 b, float);
+    void setFontType(u32 msgIndex, u32 fontType);
+    // void FUN_800b9448(u32 msgIndex, bool);
+    // void FUN_800b9460(u32 msgIndex, u8, u8, u8, float);
     void beginPrint(u32 msgIndex);
     void printf(u32 msgIndex, const char* format, ...);
-    bool printIndex(u32 msgIndex, u32 lineIndex, void* msgBinData);
+    bool printIndex(u32 msgIndex, u32 lineIndex, void* msgBinData = NULL);
     void setWindowRectVisible(u32 msgIndex, bool isVisible);
 
-
-
-    static MuMsg* create(u32 fontSetting, HeapType heapType, u32);
+    static MuMsg* create(u32 fontSetting, HeapType heapType, HeapType heapType2);
+    // CSS: HeapType 1 = 42, HeapType 2 = 43.
+    // Game: both 40
+    // Photo: both 42
+    ~MuMsg();
 };
 static_assert(sizeof(MuMsg) == 0x1c, "Class is wrong size!");
--- Brawl/Include/mu/mu_object.h
+++ Brawl/Include/mu/mu_object.h
@@ -3,8 +3,8 @@
 #include <StaticAssert.h>
 #include <gf/gf_model.h>
 #include <memory.h>
-#include <nw4r/g3d/g3d_resmdl.h>
 #include <nw4r/g3d/g3d_resfile.h>
+#include <nw4r/g3d/g3d_resmdl.h>
 #include <nw4r/g3d/g3d_scnobj.h>
 
 class MuObject {
@@ -18,22 +18,87 @@ public:
     gfModelAnimation* m_modelAnim;
     char unk2[0x44];
 
-    static MuObject* create(nw4r::g3d::ResFile* modelSource, const char* node, int drawPriority, nw4r::g3d::ResFile* textureSource, HeapType type);
-    static MuObject* create(nw4r::g3d::ResFile* modelSource, int node, int drawPriority, nw4r::g3d::ResFile* textureSource, HeapType type);
+    static MuObject* create(nw4r::g3d::ResFile* modelSource, const char* modelNode, int drawPriority, nw4r::g3d::ResFile* textureSource, HeapType type);
+    static MuObject* create(nw4r::g3d::ResFile* modelSource, int modelNode, nw4r::g3d::ResFile* textureSource, int animNode, HeapType type);
+    MuObject* copy(int modelNode, int animNode, HeapType type);
+
+    void enableSkipMatrix(bool enableSkip);
+    void enableSkipMaterial(bool enableSkip);
+    void calcMatrix();
+
     void changeAnimN(const char* animName);
     void changeNodeAnimN(const char* animName);
     void changeClrAnimN(const char* animName);
     void changeVisAnimN(const char* animName);
+    void changeTexPatAnimN(const char* animName);
+    void changeTexSrtAnimN(const char* animName);
+
     void changeAnimNIf(const char* animName);
     void changeNodeAnimNIf(const char* animName);
     void changeClrAnimNIf(const char* animName);
     void changeVisAnimNIf(const char* animName);
     void changeTexPatAnimNIf(const char* animName);
+    void changeTexSrtAnimNIf(const char* animName);
+
+    void setFrame(float frame);
     void setFrameVisible(float frame);
     void setFrameMatCol(float frame);
     void setFrameTex(float frame);
-    virtual ~MuObject();
+    void setFrameTexSrt(float frame);
+    void setFrameFlg(float frame, u8 flagFilter); // 1 CHR, 2 VIS, 4 PAT, 8 SRT, 10 CLR. SHP not supported
+    void setUpdateRateFlg(float updateRate, u8 flagFilter);
+
     void changeMaterialTex(int matNode, int texNode, nw4r::g3d::ResFile** textureSource = nullptr);
     void changeMaterialTex(int matNode, const char* textureName, nw4r::g3d::ResFile** textureSource = nullptr);
+    // void anmPlayPolicyOneTime(float, float, float);
+
+    void getNode(int objNode);
+    void getNodeID(const char* nodeName);
+    void getRect3D(Rect2D* rectangle, int nodeA, int nodeB);
+    void getRect3D(Rect2D* rectangle, const char* nodeNameA, const char* nodeNameB);
+
+    void getPos(Vec3f* pos);
+    void getPos(Vec3f* pos, const char* nodeName);
+    void setPos(Vec3f* pos);
+    void setPos(Vec3f* pos, const char* nodeName);
+    void setTrans(Vec3f* trans);
+
+    // Vec3f getGlobalPostiion(int resNode); //Ambiguous with below?
+    Vec3f getGlobalPosition(int resNode);
+    Vec3f getGlobalPosition();
+
+    void getAnimScale(Vec3f* scl, const char* name);
+    void setScale(Vec3f* scl);
+    void setScale(const char* name, Vec3f* scl);
+    void getScale(Vec3f* scl, const char* name);
+    void setObjScale(Vec3f* scl);
+    Vec3f getObjScale();
+
+    void setObjRotate(Vec3f* rot);
+    void getRotate(Vec3f* rot, const char* name);
+    void setRotate(Vec3f* rot, const char* name);
+
+    const char* getNodeAnimName();
+    const char* getVisAnimName();
+    const char* getTexPatAnimName();
+    const char* getTexSrtAnimName();
+    const char* getClrAnimName();
+
+    bool isNodeAnimFinished();
+    bool isVisAnimFinished();
+    bool isClrAnimFinished();
+    bool isAnimFinished();
+    bool isNodeAnimLoop();
+
+    virtual ~MuObject();
 };
 static_assert(sizeof(MuObject) == 0x60, "Class is wrong size!");
+
+class muObjProcessTask {
+public:
+    char unk[0x44];
+
+    static muObjProcessTask* create(HeapType type);
+    virtual ~muObjProcessTask();
+};
+static_assert(sizeof(muObjProcessTask) == 0x48, "Class is wrong size!");
