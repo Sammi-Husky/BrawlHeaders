1752f6b5e385e03d8798abdadf9282b05b2074fe
Max <34987259+mparisi20@users.noreply.github.com>
2026-02-18 21:27:09 -0500

    Match 5 new templates in so_array.h
    - soArrayContractibleTableNull
    - soArrayListUnit
    - soArrayListEnumerator
    - soEnumerator
    - soArrayList
    
    Also made various improvements to the soArray templates
    to improve matching accuracy

--- Brawl/Include/so/so_array.h
+++ Brawl/Include/so/so_array.h
@@ -30,7 +30,7 @@ public:
     virtual bool isNull() const { return false; }
     virtual T& at(s32 index) = 0;
     virtual const T& at(s32 index) const = 0;
-    virtual u32 size() const = 0;
+    virtual s32 size() const = 0;
     virtual bool isEmpty() const { return this->size() == 0; }
     virtual ~soArrayFixed() { };
 };
@@ -106,8 +106,8 @@ public:
         m_size = 0;
     }
 
-    virtual u32 size() const {
-        u32 res = m_size;
+    virtual s32 size() const {
+        s32 res = m_size;
         if (!m_elements) {
             res = 0;
         }
@@ -127,6 +127,28 @@ public:
     }
 };
 
+template<typename T>
+class soArrayContractibleTableNull : public soArrayContractible<T> {
+
+public:
+    bool isNull() const { return true; }
+    virtual T& at(s32 index) {
+        static T nullObject;
+        return nullObject;
+    }
+    virtual const T& at(s32 index) const {
+        static T nullObject;
+        return nullObject;
+    }
+    virtual s32 size() const {
+        return 0;
+    }
+    virtual ~soArrayContractibleTableNull() { }
+    virtual void shift() { }
+    virtual void pop() { }
+    virtual void clear() { }
+};
+
 template <class T>
 class soArray : public soArrayContractible<T> {
 public:
@@ -136,9 +158,9 @@ public:
     virtual void push(const T&) = 0;
     virtual void insert(u32, const T&) = 0;
     virtual void erase(u32) = 0;
-    virtual u32 capacity() = 0;
-    virtual bool isFull() = 0;
-    virtual void set(u32 startingIndex, const T& element, u32 numIndicesToSet) = 0;
+    virtual s32 capacity() const = 0;
+    virtual bool isFull() const = 0;
+    virtual void set(s32 startingIndex, const T& element, s32 numIndicesToSet) = 0;
 };
 
 template <class T>
@@ -154,7 +176,7 @@ public:
         static T m_nullElement;
         return m_nullElement;
     };
-    virtual u32 size() const { return 0; }
+    virtual s32 size() const { return 0; }
     virtual ~soArrayNull() { }
     virtual void shift() { return; }
     virtual void pop() { return; }
@@ -163,48 +185,382 @@ public:
     virtual void push(const T&) { return; }
     virtual void insert(u32, const T&) { return; }
     virtual void erase(u32) { return; }
-    virtual u32 capacity() { return 0; }
-    virtual bool isFull() { return true; }
-    virtual void set(u32 startingIndex, const T& element, u32 numIndicesToSet) { }
+    virtual s32 capacity() const { return 0; }
+    virtual bool isFull() const { return true; }
+    virtual void set(s32 startingIndex, const T& element, s32 numIndicesToSet) { }
 
     soArrayNull() { }
     soArrayNull(u32 size, u32) { }
     soArrayNull(u32 size, const T& element, u32) { }
 };
 
-template <class T, class s> // TODO: Not sure what second template argument is
-class soArrayListUnit {
-    T m_element;
-    int m_4;
-    int m_8;
+template <typename ElementTy, typename IndexTy>
+struct soArrayListUnit {
+    u8 m_inUse : 1;
+    ElementTy m_element;
+    IndexTy m_prev;
+    IndexTy m_next;
+
+    static const s32 End = -1;
+
+    soArrayListUnit() : m_inUse(false), m_prev(End), m_next(End) { }
+    ~soArrayListUnit() { }
+
+    void setNext(IndexTy n) { m_next = n; }
 };
 
-template <class T, u32 C>
-class soArrayList : public soArray<T> {
-    u32 m_a : sizeof(bit_width<C>) + 1;
-    u32 m_b : sizeof(bit_width<C>) + 1;
-    u32 m_c : sizeof(bit_width<C>) + 1;
-    u32 m_size : sizeof(bit_width<C>) + 1;
-
-    soArrayListUnit<T, int> m_units[C];
-
+template <typename T>
+class soEnumerator : public soNullable {
 public:
-    virtual bool isNull() const;
-    virtual T& at(s32 index);
-    virtual const T& at(s32 index) const;
-    virtual u32 size() const { return m_size; }
-    virtual ~soArrayList() { }
-    virtual void shift();
-    virtual void pop();
-    virtual void clear();
-    virtual void unshift(const T&);
-    virtual void push(const T&);
-    virtual void insert(u32, const T&);
-    virtual void erase(u32);
-    virtual u32 capacity() { return C; }
-    virtual bool isFull();
-    virtual void set(u32 startingIndex, const T& element, u32 numIndicesToSet);
+    virtual bool next() = 0;
+    virtual bool hasNext() const = 0;
+    virtual bool isEnd() const = 0;
+    virtual T& getCurrent() const = 0;
+    virtual ~soEnumerator() = 0;
 };
+
+template <typename ElementTy, typename IndexTy>
+struct soArrayListEnumerator : public soEnumerator<ElementTy> {
+    soArrayListUnit<ElementTy, IndexTy>* m_units;
+    s32 m_next;
+
+    static const s32 End = -1;
+public:
+    soArrayListEnumerator() : m_units(nullptr), m_next(End) { }
+    soArrayListEnumerator(soArrayListUnit<ElementTy, IndexTy>* units, s32 next) :
+        m_units(units), m_next(next) { }
+    s32 getNext() const { return m_next; }
+
+    // TODO: verify whether ft_pikmin code triggers the generation
+    // of an implicitly-defined copy-assign and copy-ctor
+
+    virtual bool next() {
+        if (!hasNext()) {
+            m_next = End;
+            return false;
+        }
+        m_next = m_units[m_next].m_next;
+        return true;
+    }
+
+    virtual bool hasNext() const {
+        return m_units[m_next].m_next >= 0;
+    }
+
+    virtual bool isEnd() const {
+        return m_next < 0;
+    }
+
+    virtual ElementTy& getCurrent() const {
+        return m_units[m_next].m_element;
+    }
+
+    virtual ~soArrayListEnumerator() { }
+};
+
+// A dynamic array of soArrayListUnits, but with a fixed maximum capcity.
+// Each unit has a logical "position" corresponding to an actual "index"
+// in the internal array. This distinction is implemented via an intrusive
+// doubly-linked list connecting the units; so getArrayIndex for details.
+template <typename T, u32 C>
+class soArrayList : public soArray<T> {
+    // The index of the next available free unit in the list
+    s32 m_freeIndex : sizeof(bit_width<C>) + 1;
+    // The index of logical position 0
+    s32 m_topIndex : sizeof(bit_width<C>) + 1;
+    // The index of logical position size() - 1
+    s32 m_lastIndex : sizeof(bit_width<C>) + 1;
+    // The current number of allocated units
+    s32 m_size : sizeof(bit_width<C>) + 1;
+
+    soArrayListUnit<T, s8> m_units[C];
+
+    // Allocate an soArrayListUnit from the internal list,
+    // incrementing the size and returning the index of the
+    // allocated unit. If idx is negative, get the next available
+    // free unit. In practice, idx is always negative
+    s32 shiftFreeArrayIndex(s32 idx);
+    // Get the index of the element at position pos
+    s32 getArrayIndex(s32 pos) const;
+    // Make space in the list such that a new element may be assigned
+    // to the returned index, if space is available. The index returned
+    // corresponds to the logical position prevPos+1
+    s32 insertSub(s32 prevPos, s32 where = End);
+    // Make all necessary updates to the internal linked list of units
+    // to erase the element at idx
+    void eraseSub(s32 idx);
+
+    s32 getFreeIndex() const { return m_freeIndex; }
+
+    // Mark the element at idx as deleted, and set idx to m_freeIndex
+    void clearElement(s32 idx) {
+        if (idx >= 0) {
+            m_units[idx].m_next = m_freeIndex;
+            m_units[idx].m_prev = End;
+            m_units[idx].m_inUse = false;
+            if (m_freeIndex > 0)
+                m_units[idx].m_prev = idx;
+            m_freeIndex = idx;
+            m_size--;
+        }
+    }
+
+    static const s32 End = -1;
+public:
+    // Note that the call to clear() immediately overwrites a 0 to m_freeIndex, so
+    // allocations begin at the start of the fixed internal array.
+    soArrayList() : m_freeIndex(End), m_topIndex(End), m_lastIndex(End) {
+        clear();
+    }
+
+    virtual bool isNull() const {
+        return false;
+    }
+
+    virtual T& at(s32 i) {
+        s32 idx = getArrayIndex(i);
+        if (this->isEmpty() != true && idx >= 0)
+            static_cast<void>(capacity());
+        return m_units[idx].m_element;
+    }
+
+    virtual const T& at(s32 i) const {
+        s32 idx = getArrayIndex(i);
+        if (this->isEmpty() != true && idx >= 0)
+            static_cast<void>(capacity());
+        return m_units[idx].m_element;
+    }
+
+    virtual s32 size() const {
+        return m_size;
+    }
+
+    virtual ~soArrayList() { }
+
+    // Erase the element in the first position
+    virtual void shift() {
+        eraseSub(getArrayIndex(0));
+    }
+
+    // Erase the element in the last position
+    virtual void pop() {
+        eraseSub(getArrayIndex(size() - 1));
+    }
+
+    // Erase all elements of the list, resetting the bitfields and linked list
+    virtual void clear() {
+        m_freeIndex = 0;
+        m_topIndex = End;
+        m_lastIndex = End;
+        m_size = 0;
+
+        if (capacity() == 1) {
+            m_units[0].m_prev = End;
+            m_units[0].m_next = End;
+            m_units[0].m_inUse = false;
+        } else {
+            for (s32 i = 0; i < capacity(); i++) {
+                m_units[i].m_prev = i - 1;
+                m_units[i].m_next = i + 1;
+                m_units[i].m_inUse = false;
+            }
+            m_units[capacity() - 1].setNext(End);
+        }
+    }
+
+    // Prepend elm to the list, if space is available
+    virtual void unshift(const T& elm) {
+        s32 idx = insertSub(-1);
+        if (idx >= 0)
+            m_units[idx].m_element = elm;
+    }
+
+    // Append elm to the list, if space is available
+    virtual void push(const T& elm) {
+        s32 idx = insertSub(size() - 1);
+        if (idx >= 0)
+            m_units[idx].m_element = elm;
+    }
+
+    // Insert elm at position i of the list, if i is in range and space is
+    // available
+    virtual void insert(u32 pos, const T& elm) {
+        s32 idx = insertSub(pos - 1);
+        if (idx >= 0)
+            m_units[idx].m_element = elm;
+    }
+
+    // Erase the element at position pos
+    virtual void erase(u32 pos) {
+        eraseSub(getArrayIndex(pos));
+    }
+
+    virtual s32 capacity() const {
+        return C;
+    }
+
+    virtual bool isFull() const {
+        return m_freeIndex < 0;
+    }
+
+    // Starting at position pos, assign elm to consecutive elements, stopping
+    // when either count or the end of the list has been reached
+    virtual void set(s32 pos, const T& elm, s32 count) {
+        if (count) {
+            if (count + pos >= size())
+                count = size() - pos;
+            s32 curr = m_topIndex;
+            while (count > 0) {
+                if (pos > 0)
+                    pos--;
+                else {
+                    m_units[curr].m_element = elm;
+                    count--;
+                }
+                if ((curr = m_units[curr].m_next) < 0)
+                    break;
+            }
+        }
+    }
+
+    // TODO: check implicit generation by <wnPikminPikmin*, 10> for these two
+    soArrayListEnumerator<T, s8> getEnumerator() {
+        return soArrayListEnumerator<T, s8>(m_units, m_topIndex);
+    }
+
+    // Erase the element at position enm.getNext()
+    void erase(const soArrayListEnumerator<T, s8>& enm) {
+        eraseSub(enm.getNext());
+    }
+
+    soArrayListUnit<T, s8>* getUnits() { return m_units; }
+};
+
+template <typename T, u32 C>
+s32 soArrayList<T, C>::shiftFreeArrayIndex(s32 idx) {
+    if (isFull() == true)
+        return End;
+    if (idx < 0) {
+        s32 freeIndex = getFreeIndex();
+        m_freeIndex = m_units[m_freeIndex].m_next;
+        if (m_freeIndex > 0)
+            m_units[m_freeIndex].m_prev = End;
+        m_size++;
+        return freeIndex;
+    }
+
+    // Note: the remainder of this function is unused, since
+    // all callers pass -1 for idx. It apparently removes the
+    // soArrayListUnit at position idx from the doubly-linked list.
+    if (idx >= capacity())
+        return End;
+    const soArrayListUnit<T, s8>& curr = m_units[idx];
+    if (static_cast<bool>(curr.m_inUse) == true)
+        return End;
+    s32 next = curr.m_next;
+    s32 prev = curr.m_prev;
+    if (prev < 0 && next < 0)
+        m_freeIndex = End;
+    else if (prev < 0) {
+        m_units[next].m_prev = End;
+        m_freeIndex = curr.m_next;
+    } else if (next < 0)
+        m_units[prev].m_next = End;
+    else {
+        m_units[prev].m_next = next;
+        m_units[next].m_prev = prev;
+    }
+    m_size++;
+    return idx;
+}
+
+template <typename T, u32 C>
+s32 soArrayList<T, C>::getArrayIndex(s32 pos) const {
+    if (pos < 0 || pos >= size())
+        return End;
+    if (pos == 0)
+        return m_topIndex;
+    if (pos == size() - 1)
+        return m_lastIndex;
+    s32 count = pos;
+    s32 curr = m_topIndex;
+    if (pos > size() / 2) {
+        // Step from the back of the list instead
+        count = size() - pos - 1;
+        curr = m_lastIndex;
+    }
+    for (s32 i = 0; i < size(); i++) {
+        if (i == count)
+            return curr;
+        if (pos > size() / 2)
+            curr = m_units[curr].m_prev;
+        else
+            curr = m_units[curr].m_next;
+        if (curr < 0)
+            break;
+    }
+    return End;
+}
+
+template <typename T, u32 C>
+s32 soArrayList<T, C>::insertSub(s32 prevPos, s32 where) {
+    s32 prevIdx = End;
+    if (prevPos >= 0) {
+        prevIdx = getArrayIndex(prevPos);
+        if (prevIdx < 0)
+            return End;
+    }
+    s32 freeIdx = shiftFreeArrayIndex(where);
+    if (freeIdx < 0)
+        return End;
+
+    s32 newNext = End;
+    if (prevIdx < 0)
+        newNext = m_topIndex;
+    else if (m_units[prevIdx].m_next >= 0)
+        newNext = m_units[prevIdx].m_next;
+
+    if (prevIdx >= 0)
+        m_units[prevIdx].m_next = freeIdx;
+    else
+        m_topIndex = freeIdx;
+
+    m_units[freeIdx].m_prev = prevIdx;
+    if (newNext >= 0)
+        m_units[newNext].m_prev = freeIdx;
+    else
+        m_lastIndex = freeIdx;
+    m_units[freeIdx].m_next = newNext;
+    m_units[freeIdx].m_inUse = true;
+    return freeIdx;
+}
+
+template <typename T, u32 C>
+void soArrayList<T, C>::eraseSub(s32 idx) {
+    if (idx < 0 || idx >= capacity())
+        return;
+    if (size() == 1) {
+        m_topIndex = End;
+        m_lastIndex = End;
+        clearElement(idx);
+    } else {
+        s32 next = getUnits()[idx].m_next;
+        s32 prev = getUnits()[idx].m_prev;
+
+        // Remove from list
+        if (prev < 0) {
+            m_units[next].m_prev = End;
+            m_topIndex = next;
+        } else if (next < 0) {
+            m_units[prev].m_next = End;
+            m_lastIndex = prev;
+        } else {
+            m_units[prev].m_next = next;
+            m_units[next].m_prev = prev;
+        }
+        clearElement(idx);
+    }
+}
 
 class soArrayVectorCalcInterface {
 public:
@@ -263,8 +619,8 @@ public:
         soArrayVectorCalculator::erase(this, index, this->size(), this->capacity(), this->getTopIndex(), this->getLastIndex());
         this->setSize(this->size() - 1);
     }
-    virtual u32 capacity() = 0;
-    virtual void set(u32 startingIndex, const T& elementToCopy, u32 numIndicesToSet) {
+    virtual s32 capacity() const = 0;
+    virtual void set(s32 startingIndex, const T& elementToCopy, s32 numIndicesToSet) {
         if (this->size() <= startingIndex + numIndicesToSet) {
             numIndicesToSet = this->size() - startingIndex;
         }
@@ -286,28 +642,23 @@ public:
     virtual void setSize(u32) = 0;
 };
 
-template <class T, u32 C>
+template <class T, s32 C>
 class soArrayVector : public soArrayVectorAbstract<T> {
-    u32 m_topIndex : sizeof(bit_width<C>) + 1;
-    u32 m_lastIndex : sizeof(bit_width<C>) + 1;
+    s32 m_topIndex : sizeof(bit_width<C>) + 1;
+    s32 m_lastIndex : sizeof(bit_width<C>) + 1;
     u32 m_size : sizeof(bit_width<C>) + 1;
-    bool m_isFull : 1;
+    u32 m_isFull : 1;
 
     T m_elements[C];
 
 public:
-    virtual u32 size() const { return m_size; }
+    virtual s32 size() const { return m_size; }
     virtual ~soArrayVector() { }
-    virtual u32 capacity() { return C; }
-    virtual bool isFull() { return m_isFull; }
+    virtual s32 capacity() const { return C; }
+    virtual bool isFull() const { return m_isFull; }
 
     virtual T& atFastAbstractSub(s32 index) const {
-        int iVar1 = this->m_topIndex + index;
-        if (C - 1 < iVar1) {
-            iVar1 -= C;
-        }
-        return const_cast<T&>(this->m_elements[iVar1]);
-
+        return atFast(index);
     }
     virtual T* getArrayValueConst(u32 index) {
         return &this->m_elements[index];
@@ -320,12 +671,8 @@ public:
     virtual void onFull() { m_isFull = true; }
     virtual void offFull() { m_isFull = false; }
 
-    soArrayVector() {
-        m_topIndex = 0;
-        m_lastIndex = 0;
-        m_size = 0;
-        m_isFull = false;
-    }
+    soArrayVector() : m_topIndex(0), m_lastIndex(0), m_size(0), m_isFull(false) { }
+
     soArrayVector(u32 size, u32) {
         m_topIndex = 0;
         m_lastIndex = 0;
@@ -342,6 +689,12 @@ public:
         for (u32 i = 0; i < size; i++) {
             this->push(element);
         }
+    }
+    T& atFast(s32 idx) const {
+        s32 i = m_topIndex + idx;
+        if (i >= C)
+            i -= C;
+        return const_cast<T&>(m_elements[i]);
     }
 };
 
