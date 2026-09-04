3e880e3c28e138e18f629637373a1cc0a1e7d242
Sammi Husky <sammi-husky@live.com>
2026-09-04 00:09:59 -0700

    Add OSUnlink and update make gfModuleHeader inherit from OSModuleHeader

--- Brawl/Include/gf/gf_module.h
+++ Brawl/Include/gf/gf_module.h
@@ -7,36 +7,8 @@
 
 // Documentation from Brawllib
 
-class gfModuleHeader {
+class gfModuleHeader : public OSModuleHeader {
 public:
-    u32 id;
-    u32 linkNext;
-    u32 linkPrev;
-    u32 numSections;
-    u32 sectionInfoOffset;
-    u32 nameOffset;
-    u32 nameSize;
-    u32 version;
-
-    u32 bssSize;
-    u32 relOffset;
-    u32 impOffset;
-    u32 impSize;
-
-    char prologSection;
-    char epilogSection;
-    char unresolvedSection;
-    char bssSection;
-
-    u32 prologOffset;
-    u32 epilogOffset;
-    u32 unresolvedOffset;
-
-    u32 moduleAlign;
-    u32 bssAlign;
-
-    u32 commandOffset;
-
     u32 getTextSectionAddr();
 };
 
